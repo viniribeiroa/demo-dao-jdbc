ad25f4cca9e30b6858b38f20acb6414bcb5cc92a
viniribeiroa <viniribeiroa@gmail.com>
2022-05-10 12:59:50 -0300

    SellerfindByDepartment

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,6 @@
 package application;
 
-import java.util.Date;
+import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -20,6 +20,14 @@ public class Program {
 		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
+		
+		System.out.println("**** TESTE 2: Seller findByDepartment ****");
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		
+		for(Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 
 }
