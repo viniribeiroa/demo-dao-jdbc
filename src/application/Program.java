3058b9f870c7ce9a859846382dee70e63c800dca
viniribeiroa <viniribeiroa@gmail.com>
2022-05-10 11:11:12 -0300

    SellerDaoJTDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,17 +2,23 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(1, "books");
+		//Department obj = new Department(1, "books");
 		
-		Seller seller = new Seller(21, "bob","bob@gmail.com",new Date(), 3000.0, obj);
+		//Seller seller = new Seller(21, "bob","bob@gmail.com",new Date(), 3000.0, obj);
 		
-		System.out.println(obj);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
 	}
 
 }
