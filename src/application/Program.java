0c5e66e67055dbbef8536ee496b99f7013cf443b
viniribeiroa <viniribeiroa@gmail.com>
2022-05-10 16:02:05 -0300

    delete

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,6 +12,8 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 		// Department obj = new Department(1, "books");
 
 		// Seller seller = new Seller(21, "bob","bob@gmail.com",new Date(), 3000.0,
@@ -55,6 +58,12 @@ public class Program {
 		sellerDao.update(seller);
 
 		System.out.println("update complete");
+		
+		System.out.println("**** TESTE 6: Seller delete ****");
+		System.out.println("entre com o id para deletar:");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("delete complete");
 
 	}
 
