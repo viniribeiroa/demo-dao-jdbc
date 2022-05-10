d8d9632e6205a8dc28df8267f415699489915433
viniribeiroa <viniribeiroa@gmail.com>
2022-05-10 11:24:57 -0300

    reutilizando a instaciação

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,6 +16,7 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		System.out.println("**** TESTE 1: Seller findById ****");
 		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
