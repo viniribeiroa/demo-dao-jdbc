297e541e209bf378c76d4a9aa3a6fcd374e57a10
viniribeiroa <viniribeiroa@gmail.com>
2022-05-11 11:39:11 -0300

    DepartmentDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,10 @@
 package application;
 
-import java.util.Date;
-import java.util.List;
 import java.util.Scanner;
 
 import model.dao.DaoFactory;
-import model.dao.SellerDao;
+import model.dao.DepartmentDao;
 import model.entities.Department;
-import model.entities.Seller;
 
 public class Program {
 
@@ -19,7 +16,7 @@ public class Program {
 		// Seller seller = new Seller(21, "bob","bob@gmail.com",new Date(), 3000.0,
 		// obj);
 
-		SellerDao sellerDao = DaoFactory.createSellerDao();
+		/*SellerDao sellerDao = DaoFactory.createSellerDao();
 
 		System.out.println("**** TESTE 1: Seller findById ****");
 		Seller seller = sellerDao.findById(3);
@@ -63,7 +60,14 @@ public class Program {
 		System.out.println("entre com o id para deletar:");
 		int id = sc.nextInt();
 		sellerDao.deleteById(id);
-		System.out.println("delete complete");
+		System.out.println("delete complete");*/		
+		
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		Department newDepartment = new Department(null, "Comercial");
+		departmentDao.insert(newDepartment);
+
+		System.out.println("insert new id = " + newDepartment.getId());
+		sc.close();
 
 	}
 
